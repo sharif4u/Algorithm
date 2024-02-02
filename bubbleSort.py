e5ac43b01dda08a45812911c539a9736d4d9d2fa
MD. SHARIF UDDIN <101212925+sharif4u@users.noreply.github.com>
2024-02-03 00:56:17 +0600

    Create bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,13 @@
+#Buuble sort using python
+def bubbleSort(array):
+  for i in range(len(array)):
+    for j in range(0,len(array)-i-1):
+      if array[j] > array[j+1]:
+        array[j] += array[j+1]
+        array[j+1] = array[j] - array[j+1]
+        array[j] = array[j] - array[j+1]
+arrayData = [1,43,56,45,56,5,3,4,5,23,65,75]
+
+bubbleSort(arrayData)
+print("Sorted arrray in achending order: ")
+print(arrayData)
