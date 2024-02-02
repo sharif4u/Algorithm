aa91f96627c6ccf8687a3fc8412cbbace7e48f1a
MD. SHARIF UDDIN <101212925+sharif4u@users.noreply.github.com>
2024-02-03 01:06:38 +0600

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -11,3 +11,22 @@ arrayData = [1,43,56,45,56,5,3,4,5,23,65,75]
 bubbleSort(arrayData)
 print("Sorted arrray in achending order: ")
 print(arrayData)
+
+#another for time complexity
+#Buuble sort using python
+def bubbleSort(array):
+  for i in range(len(array)):
+    swapped = False
+    for j in range(0,len(array)-i-1):
+      if array[j] > array[j+1]:
+        array[j] += array[j+1]
+        array[j+1] = array[j] - array[j+1]
+        array[j] = array[j] - array[j+1]
+        swapped = True
+    if not swapped:
+      break
+arrayData = [1,43,56,45,56,5,3,4,5,11,65,75]
+
+bubbleSort(arrayData)
+print("Sorted arrray in achending order: ")
+print(arrayData)
